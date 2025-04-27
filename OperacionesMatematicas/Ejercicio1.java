d411a3bf7b4643772335494e9b50b43bb6d19eb3
YolianYamaha <yandresbarona@estudiante.uniajc.edu.co>
2025-04-27 21:51:33 +0000

    OperacionesMatematicascompleta

--- OperacionesMatematicas/Ejercicio1.java
+++ OperacionesMatematicas/Ejercicio1.java
@@ -1,11 +1,14 @@
-public class Main {
+public class Ejercicio1 {
     public static void main(String[] args) {
         OperacionesMatematicas op = new OperacionesMatematicas();
 
-        double suma = op.sumar(10, 5);
-        double resta = op.restar(10, 5);
-        double multiplicacion = op.multiplicar(10, 5);
-        double division = op.dividir(10, 0); // mostrará error por división por cero
+        double a = 10.5;
+        double b = 3.25;
+
+        double suma = op.sumar(a, b);
+        double resta = op.restar(a, b);
+        double multiplicacion = op.multiplicar(a, b);
+        double division = op.dividir(a, b);
 
         System.out.println("Suma: " + suma);
         System.out.println("Resta: " + resta);
